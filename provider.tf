89b600a8f3f6ff4506dd1cd71890306b95ec78cd
Anuj <anuj8ranchi@gmail.com>
2023-07-07 12:25:35 +0530

    added all files

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the New Relic provider
 provider "newrelic" {
-#   account_id = <Your Account ID>
-#   api_key = <Your User API Key>    # Usually prefixed with 'NRAK'
+  account_id = <Your Account ID>
+  api_key = <Your User API Key>    # Usually prefixed with 'NRAK'
   region = "US"                    # Valid regions are US and EU
 }
